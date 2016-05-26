02ffb6fb58a421e9397abfc4eb57afe00df22c39
María M. Vargas Aponte <maria.vargas.aponte@gmail.com>
2016-05-26 00:48:56 -0400

    Guess the complete word added

--- Hangman/Hangman.hpp
+++ Hangman/Hangman.hpp
@@ -20,10 +20,12 @@ class Hangman {
     static const int MAX_LEN = maxArrayLegth;
     static const int MAX_LETTER_LENGTH = maxNumberOFGuessedLetters;
     
+    bool activeGame;
     string selectedWord;
     int numberOfLettersInWord = 0;
-    char guessedLetters[MAX_LETTER_LENGTH];
-    char wordArray[MAX_LEN];
+    char guessedLetters[MAX_LETTER_LENGTH] = {0};
+    char wordArray[MAX_LEN] = {0};
+    int numLetters = 0;
     int numberOfLettersUsed = 0;
     int numberOfFailedAttempts = 0;
     int maxFailedAttemps = 0;
@@ -36,16 +38,18 @@ public:
     ~Hangman(void);
     
     void menu();
+    bool startGame();
     string getWord();
     void setWord(string word);
     int getNumberOfLettersInWord();
     void setNumberOfLettersInWord(int number);
     int calculateNumberOfLettersInWord();
     void askForLetter();
-    void checkIfWordContainsLetter(char letter);
-    
-    bool startGame();
+    void checkIfWordContainsLetter(char letter, int wordSize);
+    void printFoundLetters(char array[], int size);
     void drawLetterLines(int number);
+    bool checkIfWordIsGuessed(char array[], int size);
+    bool guessWord();
 };
 
 
